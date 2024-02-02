0bb1c5e20b4d065c078d68eec628be5f975d40cd
HeniSapta <152345387+HeniSapta@users.noreply.github.com>
2024-02-02 11:31:34 +0700

    Add files via upload

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic
+
+namespace LatihanList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listNilai = new list<int>();
+
+            listNilai.Add(70);
+            listNilai.Add(100);
+            listNilai.Add(85);
+
+            int nilaiElement1 = listNilai[1];
+
+            Console.WriteLine(nilaiElement1);
+            Console.WriteLine();
+
+            foreach (int nilai in listNilai)
+            {
+                Console.WriteLine(nilai);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
